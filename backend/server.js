e432b3737e9cd71da5cc5a1aa2ce274ed97735cb
sxr <sxr@sxr.local>
2024-11-18 08:22:42 +0800

    start before mongod

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,22 @@
 // cors dotenv body-parser multer stripe validator nodemon 
 //   express mongoose jsonwebtoken bcrypt 
  
-// 3:28
+import express from 'express';
+import cors from 'cors';
+
+// app config
+const app = express(); 
+const port = 4000;
+
+// middlewares
+app.use(express.json());
+// cors 链接前端
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("API working");
+})
+
+app.listen(port, () => {
+    console.log(`Listening on  http://localhost:${port}`);
+})
